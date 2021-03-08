4cfd6c17c12e6d1121dab20e7f53c29c0e0166a2
Andrew <fakmaki99@mail.ru>
2021-03-08 22:43:52 +0500

    Pin code verification implemented

---
+++ AmazingApp/PinCodeVer/PinCodeVerificator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace AmazingApp.PinCodeVer
+{
+    class PinCodeVerificator : IPinCodeVerificator
+    {
+        public bool VerifyPin(string pinCode)
+        {
+            if (pinCode.Any(pinCharacter => !char.IsDigit(pinCharacter)))
+            {
+                return false;
+            }
+
+            return pinCode.Length == 4 || pinCode.Length == 6;
+        }
+    }
+}
