1a18585e2716a8063b480b5b745fa3ee6dd69163
MidnightSun27 <54239122+MidnightSun27@users.noreply.github.com>
2021-03-08 23:06:40 +0500

    Update BoobleIntSort.cs

--- AmazingApp/BoobleSort/BoobleIntSort.cs
+++ AmazingApp/BoobleSort/BoobleIntSort.cs
@@ -1,14 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AmazingApp.BoobleSort
 {
-    class BoobleIntSort:IBoobleIntSort
+    public class BoobleIntSort : IBoobleIntSort
     {
         public int[] Sort(int[] array)
         {
-            throw new NotImplementedException();
+            if (array.Length == 0)
+                return array;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        (array[i], array[j]) = (array[j], array[i]);
+                    }
+                }
+            }
+
+            return array;
         }
     }
 }
